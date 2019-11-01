dc398da832b9f9d31ea15297bfa166e18733090b
Daniel Medeiros <daniel@arneam.com>
2019-11-01 05:25:27 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Num: Config
+
+Simple Spring Cloud Config server for 'Num' solution.
---
+++ src/test/java/com/arneam/numconfig/NumConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arneam.numconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
